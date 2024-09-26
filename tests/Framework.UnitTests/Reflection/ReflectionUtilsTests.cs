762e5fd045d13a5646ff44230704f1d2b185d90c
Tourmi <ragtag_devilstick@tourmi.dev>
2024-09-25 23:08:12 -0400

    Fix GetImplementingTypes returning private types

---
+++ tests/Framework.UnitTests/Reflection/ReflectionUtilsTests.cs
@@ -0,0 +1,27 @@
+﻿#pragma warning disable CA1812 // Avoid uninstantiated internal classes
+
+namespace Tourmi.Framework.Reflection;
+
+[TestFixture(TestOf = typeof(ReflectionUtils))]
+internal class ReflectionUtilsTests
+{
+    [Test]
+    public void GetImplementingTypesReturnsPublicFullyImplementedTypes()
+    {
+        Assert.That(ReflectionUtils.GetImplementingTypes<ITestInterface>(), Is.EquivalentTo(new Type[] { typeof(Implementation) }));
+    }
+
+    public class Implementation : AbstractImplementation;
+
+    private class PrivateImplementation : AbstractImplementation;
+
+    public abstract class AbstractImplementation : ITestInterface
+    {
+        public int SomeProperty { get; set; }
+    }
+
+    public interface ITestInterface
+    {
+        public int SomeProperty { get; set; }
+    }
+}
